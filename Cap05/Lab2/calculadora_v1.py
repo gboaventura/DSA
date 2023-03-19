4adc56991ccb0dcc69b2d4cf6e350fc3a605c792
Gustavo Boaventura <44546580+gboaventura@users.noreply.github.com>
2023-03-19 16:15:00 +0100

    Remove starts

--- Cap05/Lab2/calculadora_v1.py
+++ Cap05/Lab2/calculadora_v1.py
@@ -3,6 +3,6 @@
 # Desenvolva uma calculadora em Python com tudo que você aprendeu nos capítulos até aqui no curso. 
 # A solução será apresentada no próximo capítulo!
 
-print("\n******************* Calculadora em Python *******************")
+print("\n****************** Calculadora em Python ******************")
 
 
